f57e543a111a200126d95e81167148fb6b1602af
w1qdev <56930814+w1qdev@users.noreply.github.com>
2023-11-03 00:05:34 +0500

    Update AuthPage.jsx

--- admin/src/pages/AuthPage/AuthPage.jsx
+++ admin/src/pages/AuthPage/AuthPage.jsx
@@ -48,7 +48,7 @@ export default function AuthPage() {
                 initial={{ y: 50, opacity: 0 }} 
                 animate={{ y: 0, opacity: 1 }} 
                 exit={{ y: 50, opacity: 0 }}
-                transition={{ duration: 0.6 }} 
+                transition={{ duration: 0.3 }} 
             >
                 <div className="auth__container__header">
                     <div className="header__company-logo">URTK Airlines</div>
